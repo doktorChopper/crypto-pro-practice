b68f1881d0946bd58c2b85e5920e504d54526d76
Chopper <doc.chopperx@gmail.com>
2025-06-17 02:33:17 +0300

    something fix

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ static HCRYPTKEY hKey = 0;
 static PCERT_PUBLIC_KEY_INFO pPubKeyInfo = NULL;
 static BYTE *pbSignature = NULL;
 static BYTE *pbHash = NULL;
+static HCRYPTHASH hHash = 0;
 
 /*#define CONTAINER _TEXT("\\\\.\\HDIMAGE\\test")*/
 
@@ -24,9 +25,9 @@ static BYTE *pbHash = NULL;
 static void CleanUp(void);
 static void HandleError(const char *);
 
-void Signing();
-void CheckSigning();
-
+static void signHash(DWORD *);
+static void checkSign(BYTE *, DWORD, DWORD);
+/*void CheckSigning();*/
 
 
 int main(int argc, char *argv[]) {
@@ -36,7 +37,6 @@ int main(int argc, char *argv[]) {
     FILE *hFile;
     LPSTR pszUserName;
     DWORD dwUserNameLen;
-    HCRYPTHASH hHash = 0;
     DWORD cbHash = 0;
     DWORD dwSigLen;
     DWORD dwInfoLen;
@@ -45,22 +45,18 @@ int main(int argc, char *argv[]) {
     BYTE *pbBuffer = (BYTE *)"The data that is to be hashed and signed.";
     DWORD dwBufferLen = (DWORD)(strlen((char *)pbBuffer + 1));
 
-    if(argc != 2 || argv[1] == NULL)
-        HandleError("input filename");
-
-    if(!(hFile = fopen(argv[1], "r+b"))) {
-        char s[BUFSIZE];
-        if(snprintf(s, BUFSIZE, "could not open file %s", argv[1]) < 0)
-            printf("error snprintf");
-        HandleError(s);
-    }
-    printf("The file %s was opened.\n", argv[1]);
-
-    /*if(CryptAcquireContextA(&hProv, "TEST", NULL, PROV_GOST_2012_256, CRYPT_VERIFYCONTEXT)) {*/
-    /*    printf("CSP is acquired!\n");*/
+    /*if(argc != 2 || argv[1] == NULL)*/
+    /*    HandleError("input filename");*/
+    /**/
+    /*if(!(hFile = fopen(argv[1], "r+b"))) {*/
+    /*    char s[BUFSIZE];*/
+    /*    if(snprintf(s, BUFSIZE, "could not open file %s", argv[1]) < 0)*/
+    /*        printf("error snprintf");*/
+    /*    HandleError(s);*/
     /*}*/
+    /*printf("The file %s was opened.\n", argv[1]);*/
 
-    if(CryptAcquireContextA(&hProv, CONTAINER, NULL, PROV_GOST_2012_256, 0))
+    if(CryptAcquireContextA(&hProv, CONTAINER, NULL, PROV_EC_CURVE25519, 0))
         printf("CSP is acquired!\n");
     else
         HandleError("Error during CryptAcquireContext");
@@ -78,44 +74,6 @@ int main(int argc, char *argv[]) {
         printf("Contents have been written to the CERT_PUBLIC_KEY_INFO.\n");
     else
         HandleError("Error during CryptExportPublicKeyInfo for signkey.");
-
-    // CalcHash
-
-    /*DWORD cbRead = 0;*/
-    /*BYTE file[BUFSIZE];*/
-    /**/
-    /*if(!CryptCreateHash(hProv, CALG_SHA_512, 0, 0, &hHash))*/
-    /*    HandleError("CryptCreateHash failed!");*/
-    /*do {*/
-    /*    cbRead = (DWORD) fread(file, 1, BUFSIZE, hFile);*/
-    /**/
-    /*    if(cbRead) {*/
-    /*        if(!CryptHashData(hHash, file, BUFSIZE, 0)) {*/
-    /*            CryptDestroyHash(hHash);*/
-    /*            HandleError("CryptHashData failed");*/
-    /*        }*/
-    /*    }*/
-    /*} while (!feof(hFile));*/
-    /**/
-    /*cbHash = SHA512LEN;*/
-    /*if(!CryptGetHashParam(hHash, HP_HASHVAL, fHash, &cbHash, 0)) {*/
-    /*    CryptDestroyHash(hHash);*/
-    /*    CryptReleaseContext(hProv, 0);*/
-    /*    HandleError("CryptGetHashParam failed"); */
-    /*}*/
-    /**/
-    /*printf("SHA512 hash of file %s is: ", argv[1]);*/
-    /*for(int i = 0; i < cbHash; i++) {*/
-    /*    printf("%c%c", fDigits[fHash[i] >> 4], fDigits[fHash[i] & 0xf]);*/
-    /*}*/
-    /*printf("\n");*/
-    /**/
-    /*dwSigLen = 0;*/
-    /*if(CryptSignHashA(hHash, AT_SIGNATURE, NULL, 0, NULL, &dwSigLen))*/
-    /*    printf("Signature lenght %d found.\n", dwSigLen);*/
-    /*else*/
-    /*    HandleError("Error during CryptSignHash.");*/
-
 
     if(CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, &hHash))
         printf("Hash object created.\n");
@@ -141,20 +99,7 @@ int main(int argc, char *argv[]) {
     else
         HandleError("Error during CryptHashData");
 
-    dwSigLen = 0;
-    if(CryptSignHash(hHash, AT_SIGNATURE, NULL, 0, NULL, &dwSigLen))
-        printf("Signature lenght %d found.\n", dwSigLen);
-    else
-        HandleError("Error during CryptSignHash");
-
-    pbSignature = (BYTE *) malloc(dwSigLen);
-    if(!pbSignature)
-        HandleError("Out of memory.");
-
-    if(CryptSignHash(hHash, AT_SIGNATURE, NULL, 0, pbSignature, &dwSigLen))
-        printf("pbSignature is the hash signature.\n");
-    else
-        HandleError("Error during CryptSignHash.");
+    signHash(&dwSigLen);
 
     if(!(signature = fopen("signature.txt", "w+b")))
         HandleError("Problem opening the file signature.txt\n");
@@ -168,13 +113,28 @@ int main(int argc, char *argv[]) {
     printf("The hash object has been destroyed.\n");
     printf("The signing phase of this program is completed.\n\n");
 
-    fclose(hFile);
+    /*fclose(hFile);*/
+
+
+
+    /*signHash(&dwSigLen);*/
+    checkSign(pbBuffer, dwBufferLen, dwSigLen);
     CleanUp();
 
     return 0;
 }
 
 void CleanUp(void) {
+
+    free(pbSignature);
+    free(pbHash);
+    free(pPubKeyInfo);
+
+    if(hHash)
+        CryptDestroyHash(hHash);
+
+    if(hKey)
+        CryptDestroyKey(hKey);
 
     if(hProv)
         CryptReleaseContext(hProv, 0);
@@ -188,4 +148,49 @@ void HandleError(const char *s)
     CleanUp();
     if(!err) err = 1;
     exit(err);
+}
+
+static void checkSign(BYTE *pbBuffer, DWORD dwBufferLen, DWORD dwSigLen) {
+    
+    if(CryptImportPublicKeyInfo(hProv, X509_ASN_ENCODING, pPubKeyInfo, &hKey))
+        printf("The key has been imported.\n");
+    else
+        HandleError("Public key import failed");
+
+    if(CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, &hHash))
+        printf("The hash object has been recreated.\n");
+    else
+        HandleError("Error during CryptCreateHash.");
+
+    if(CryptHashData(hHash, pbBuffer, dwBufferLen, 0))
+        printf("The new hash has been created.\n");
+    else
+        HandleError("Error during CryptHashData");
+
+    if(CryptVerifySignature(hHash, pbSignature, dwSigLen, hKey, NULL, 0))
+        printf("The signature has been verified.\n");
+    else
+        HandleError("Signature not validate!\n");
+
+    if(hHash)
+        CryptDestroyHash(hHash);
+}
+
+
+static void signHash(DWORD *dwSigLen) {
+
+    *dwSigLen = 0;
+    if(CryptSignHash(hHash, AT_SIGNATURE, NULL, 0, NULL, dwSigLen))
+        printf("Signature lenght %d found.\n", *dwSigLen);
+    else
+        HandleError("Error during CryptSignHash");
+
+    pbSignature = (BYTE *) malloc(*dwSigLen);
+    if(!pbSignature)
+        HandleError("Out of memory.");
+
+    if(CryptSignHash(hHash, AT_SIGNATURE, NULL, 0, pbSignature, dwSigLen))
+        printf("pbSignature is the hash signature.\n");
+    else
+        HandleError("Error during CryptSignHash.");
 }
