8e205b1c66a04d75c487915ab986be7e9594b620
Chopper <doc.chopperx@gmail.com>
2025-06-17 21:18:18 +0300

    add verify method

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 
 
 static HCRYPTPROV hProv = 0;
-static HCRYPTKEY hKey = 0;
+static HCRYPTKEY hPubKey = 0;
 static HCRYPTHASH hHash = 0;
 
 static BYTE *pbSignature = NULL;
@@ -58,23 +58,92 @@ int main(int argc, char * argv[]) {
 
 
     } else if(!strcmp(argv[1], "--verify")) {
-        exit(1);
+        verifySignature(argv[2], argv[3]);
     }
-
 
     cleanUp();
 
     return 0;
 }
 
-static void verifySignature(const char * signature, const char * filename) {
+static void verifySignature(const char * sig, const char * filename) {
+
+    FILE * fin;
+    FILE * pubKey;
+    FILE * signature;
+
+    DWORD dwSigLen;
+    DWORD dwInfoLen;
+    DWORD cbHash = 0;
+
+    if(!(pubKey = fopen("pubKey.txt", "r+b"))) {
+        fclose(pubKey);
+        handleError("Problem opening the file pubKey.txt\n");
+    }
     
+    long ogPosPub = ftell(pubKey);
+    fseek(pubKey, 0, SEEK_END);
+    dwInfoLen = ftell(pubKey);
+    fseek(pubKey, ogPosPub, SEEK_SET);
+
+    printf("%d\n", dwInfoLen);
+
+    fread(pPubKeyInfo, 1, dwInfoLen, pubKey);
+
+    if(!CryptImportPublicKeyInfo(hProv, X509_ASN_ENCODING, pPubKeyInfo, &hPubKey))
+        handleError("Public key import failed!");
+    printf("The key has been imported.\n");
+
+    if(!CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, &hHash))
+        handleError("Error during created hash.");
+    printf("Hash object created.\n");
+
+    if(!CryptGetHashParam(hHash, HP_OID, NULL, &cbHash, 0))
+        handleError("Error during CryptGetHashParam.");
+    printf("Size of the BLOB determined.\n");
+
+    pbHash = (BYTE *)malloc(cbHash);
+    if(!pbHash)
+        handleError("Out of memory.\n");
+
+    if(!CryptGetHashParam(hHash, HP_OID, pbHash, &cbHash, 0))
+        handleError("Error during CryptGetHashParam.");
+    printf("Parameters have been written to the pbHash.\n");
+
+    DWORD cbRead;
+    BYTE chFile[BUFSIZE];
+
+    do {
+        cbRead = (DWORD) fread(chFile, 1, BUFSIZE, fin);
+
+        if(cbRead) {
+            if(!CryptHashData(hHash, chFile, cbRead, 0))
+                handleError("CryptHashData failed!"); // TODO закрыть файл?
+        }
+
+    } while(!feof(fin));
+
+    if(!(signature = fopen("signature.txt", "r+b"))) {
+        fclose(signature);
+        fclose(fin);
+        handleError("Problem opening the file signature.txt\n");
+    }
+
+    long ogPos = ftell(signature);
+    fseek(signature, 0, SEEK_END);
+    dwSigLen = ftell(signature);
+    fseek(signature, ogPos, SEEK_SET);
+
+    if(!CryptVerifySignature(hHash, pbSignature, dwSigLen, hPubKey, NULL, 0))
+        handleError("Signature not validated!\n");
+    printf("The signature has been verified!\n");
 }
 
 static void signData(const char * filename) {
 
     FILE * fin;
     FILE * signature;
+    FILE * pubKey;
 
     DWORD dwInfoLen;
     DWORD dwSigLen;
@@ -91,20 +160,35 @@ static void signData(const char * filename) {
     }
     printf("The file %s was opened.\n", filename);
 
-    if(!CryptExportPublicKeyInfo(hProv, AT_SIGNATURE, X509_ASN_ENCODING, NULL, &dwInfoLen)) 
+    if(!CryptExportPublicKeyInfo(hProv, AT_SIGNATURE, X509_ASN_ENCODING, NULL, &dwInfoLen)) {
+        fclose(fin);
         handleError("Error during CryptExportPublicKeyInfo for signkey.");
+    }
     printf("Size of the CERT_PUBLIC_KEY_INFO determined.\n");
 
     pPubKeyInfo = (PCERT_PUBLIC_KEY_INFO) malloc(dwInfoLen);
     if(!pPubKeyInfo)
         handleError("Out of memory.\n");
 
-    if(!CryptExportPublicKeyInfo(hProv, AT_SIGNATURE, X509_ASN_ENCODING, pPubKeyInfo, &dwInfoLen))
+    if(!CryptExportPublicKeyInfo(hProv, AT_SIGNATURE, X509_ASN_ENCODING, pPubKeyInfo, &dwInfoLen)) {
+        fclose(fin);
         handleError("Error during CryptExportPublicKeyInfo for signkey.");
+    }
     printf("Contents have been written to the CERT_PUBLIC_KEY_INFO.\n");
 
-    if(!CryptCreateHash(hProv, CALG_ED25519, 0, 0, &hHash))
+    if(!(pubKey = fopen("pubKey.txt", "w+b"))) {
+        fclose(pubKey);
+        fclose(fin);
+        handleError("Problem opening the file pubKey.txt\n");
+    }
+
+    fwrite(pPubKeyInfo, 1, dwInfoLen, pubKey);
+    fclose(pubKey);
+
+    if(!CryptCreateHash(hProv, CALG_GR3411_2012_256, 0, 0, &hHash)) {
+        fclose(fin);
         handleError("Error during created hash.");
+    }
     printf("Hash object created.\n");
 
     if(!CryptGetHashParam(hHash, HP_OID, NULL, &cbHash, 0))
